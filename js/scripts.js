d0e96beb1b905c950fd6d1b302f08a7772f025b5
Andrew Haupt & Alisa Colon <student@epicodus.com>
2015-09-10 16:13:24 -0700

    Did all the things

--- js/scripts.js
+++ js/scripts.js
@@ -1,86 +1,25 @@
-var ones = function(number) {
-  var onesNumber = ""
+var wordOrder = function(string) {
+  var words = string.split(" ")
+  var completedWords = {}
+  var sortedWords = {}
 
-  if ((number < 4) && (number !== 0)) {
-    for (var i = 0; i < number; i++) {
-      onesNumber += "I"
+  var i = 0
+
+  while (i < words.length) {
+    var i2 = i + 1
+    var counter = 1
+    while (i2 < words.length) {
+      if (words[i] === words[i2]) {
+        counter += 1
+      }
+      i2++
     }
-  } else if (number === 4) {
-    onesNumber = "IV"
-  } else if (number === 5) {
-    onesNumber = "V"
-  } else if ((number > 5) && (number < 9)) {
-    onesNumber = "V"
-    for (var i = 0; i < number - 5; i++) {
-      onesNumber += "I"
+    if (!completedWords.hasOwnProperty(words[i])) {
+      completedWords[words[i]] = counter
     }
-  } else if (number === 9) {
-      onesNumber = "IX"
+    i++
   }
-  return onesNumber
+
+  return completedWords
 
 }
-
-var tens = function(number) {
-  var tensNum = ones(number)
-  tensNum = tensNum.replace(/V/g, "L")
-  tensNum = tensNum.replace(/X/g, "C")
-  tensNum = tensNum.replace(/I/g, "X")
-  return tensNum
-}
-
-var hundreds = function(number) {
-  var hundredsNum = tens(number)
-  hundredsNum = hundredsNum.replace(/L/g, "D")
-  hundredsNum = hundredsNum.replace(/C/g, "M")
-  hundredsNum = hundredsNum.replace(/X/g, "C")
-  return hundredsNum
-}
-
-var thousands = function(number) {
-  var thousandsNum = ones(number)
-  thousandsNum = thousandsNum.replace(/I/g, "M")
-  return thousandsNum
-}
-
-var romanNumerals = function(number) {
-  if (number > 3999) {
-    return "Not Roman, Dude!"
-  }
-  var splitNumber = number.toString().split("").map(function(x){return parseInt(x)});
-  var last = splitNumber.length - 1
-  var romanized = ""
-
-  if (splitNumber.length > 3) {
-    romanized += thousands(splitNumber[last - 3])
-  }
-  if (splitNumber.length > 2) {
-    romanized += hundreds(splitNumber[last - 2])
-  }
-  if (splitNumber.length > 1) {
-    romanized += tens(splitNumber[last - 1])
-  }
-  if (splitNumber.length > 0) {
-    romanized += ones(splitNumber[last])
-  }
-
-  return romanized
-
-}
-
-$(document).ready(function() {
-  $('form#roman-numerals').submit(function(event){
-    var number = parseInt($('input#number').val())
-    var result = romanNumerals(number)
-
-    $('.roman-numerals').text(result)
-
-    $('.ui.modal')
-    .modal('show');
-    
-    $('#result').show()
-    event.preventDefault()
-  })
-
-
-})
--- spec/specs.js
+++ spec/specs.js
@@ -1,27 +1,5 @@
-describe('ones', function() {
-  it('converts roman numerals up to 10', function() {
-    expect(ones(8)).to.equal("VIII");
-    expect(ones(4)).to.equal("IV");
-  });
-});
-
-describe('romanNumerals', function() {
-  it('converts roman numerals up to 99', function() {
-    expect(romanNumerals(50)).to.equal("L");
-    expect(romanNumerals(85)).to.equal("LXXXV");
-  });
-});
-
-describe('romanNumerals', function() {
-  it('converts roman numerals up to 999', function() {
-    expect(romanNumerals(987)).to.equal("CMLXXXVII");
-    expect(romanNumerals(132)).to.equal("CXXXII");
-  });
-});
-
-describe('romanNumerals', function() {
-  it('converts roman numerals up to 3999', function() {
-    expect(romanNumerals(3987)).to.equal("MMMCMLXXXVII");
-    expect(romanNumerals(5132)).to.equal("Not Roman, Dude!");
+describe('wordOrder', function() {
+  it('lists words in order by how many times the word appears', function() {
+    expect(wordOrder("Alisa is Alisa Andrew Andrew Andrew")).to.eql({Andrew: 3, Alisa: 2, is: 1});
   });
 });
