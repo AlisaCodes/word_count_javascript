74ae8a78452da88329f1203d7a57e5bb404098f4
Alisa Colon & Andrew Haupt <student@epicodus.com>
2015-09-10 13:38:54 -0700

    Style html page

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,86 @@
-var countUpBy = function(number1, number2) {
-  var multiples = []
+var ones = function(number) {
+  var onesNumber = ""
 
-  for(var index = number2; index <= number1; index += number2) {
-    multiples.push(index)
+  if ((number < 4) && (number !== 0)) {
+    for (var i = 0; i < number; i++) {
+      onesNumber += "I"
+    }
+  } else if (number === 4) {
+    onesNumber = "IV"
+  } else if (number === 5) {
+    onesNumber = "V"
+  } else if ((number > 5) && (number < 9)) {
+    onesNumber = "V"
+    for (var i = 0; i < number - 5; i++) {
+      onesNumber += "I"
+    }
+  } else if (number === 9) {
+      onesNumber = "IX"
   }
-  return multiples
+  return onesNumber
+
+}
+
+var tens = function(number) {
+  var tensNum = ones(number)
+  tensNum = tensNum.replace(/V/g, "L")
+  tensNum = tensNum.replace(/X/g, "C")
+  tensNum = tensNum.replace(/I/g, "X")
+  return tensNum
+}
+
+var hundreds = function(number) {
+  var hundredsNum = tens(number)
+  hundredsNum = hundredsNum.replace(/L/g, "D")
+  hundredsNum = hundredsNum.replace(/C/g, "M")
+  hundredsNum = hundredsNum.replace(/X/g, "C")
+  return hundredsNum
+}
+
+var thousands = function(number) {
+  var thousandsNum = ones(number)
+  thousandsNum = thousandsNum.replace(/I/g, "M")
+  return thousandsNum
+}
+
+var romanNumerals = function(number) {
+  if (number > 3999) {
+    return "Not Roman, Dude!"
+  }
+  var splitNumber = number.toString().split("").map(function(x){return parseInt(x)});
+  var last = splitNumber.length - 1
+  var romanized = ""
+
+  if (splitNumber.length > 3) {
+    romanized += thousands(splitNumber[last - 3])
+  }
+  if (splitNumber.length > 2) {
+    romanized += hundreds(splitNumber[last - 2])
+  }
+  if (splitNumber.length > 1) {
+    romanized += tens(splitNumber[last - 1])
+  }
+  if (splitNumber.length > 0) {
+    romanized += ones(splitNumber[last])
+  }
+
+  return romanized
+
 }
 
 $(document).ready(function() {
-  $("form#count-up-by").submit(function(event) {
-    var number1 = parseInt($("input#number1").val())
-    var number2 = parseInt($("input#number2").val())
-    var result = countUpBy(number1, number2)
+  $('form#roman-numerals').submit(function(event){
+    var number = parseInt($('input#number').val())
+    var result = romanNumerals(number)
 
-    $(".number-array").text(result)
+    $('.roman-numerals').text(result)
 
-    $("#result").show()
+    $('.ui.modal')
+    .modal('show');
+    
+    $('#result').show()
     event.preventDefault()
   })
+
+
 })
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,27 @@
-describe('countUpBy', function() {
-  it('counts up to a specific number by multiples of another number', function() {
-    expect(countUpBy(30,5)).to.eql([5,10,15,20,25,30])
+describe('ones', function() {
+  it('converts roman numerals up to 10', function() {
+    expect(ones(8)).to.equal("VIII");
+    expect(ones(4)).to.equal("IV");
+  });
+});
+
+describe('romanNumerals', function() {
+  it('converts roman numerals up to 99', function() {
+    expect(romanNumerals(50)).to.equal("L");
+    expect(romanNumerals(85)).to.equal("LXXXV");
+  });
+});
+
+describe('romanNumerals', function() {
+  it('converts roman numerals up to 999', function() {
+    expect(romanNumerals(987)).to.equal("CMLXXXVII");
+    expect(romanNumerals(132)).to.equal("CXXXII");
+  });
+});
+
+describe('romanNumerals', function() {
+  it('converts roman numerals up to 3999', function() {
+    expect(romanNumerals(3987)).to.equal("MMMCMLXXXVII");
+    expect(romanNumerals(5132)).to.equal("Not Roman, Dude!");
   });
 });
